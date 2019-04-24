469013291f5633f696cfd7617a5262db30988657
mark <mark@protopie.io>
2019-04-24 19:49:46 +0900

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1 v-my-directive.haha="'green'">{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import myDirective from "../directives/my-directive";
+
+@Component({
+  directives: {
+    "my-directive": myDirective
+  }
+})
+export default class HelloWorld extends Vue {
+  @Prop() msg!: string;
+}
+</script>
